d119d906a064d84070628e060b892f4197589cba
César O. Araújo <oaraujocesar@gmail.com>
2021-12-29 14:33:21 -0300

    style: :art: Set up webfont Poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,35 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 300;
+    src: local(''),
+      url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 400;
+    src: local(''),
+      url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
+
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 600;
+    src: local(''),
+      url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -15,7 +44,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
