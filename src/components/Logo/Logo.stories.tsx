91bc902ab3e045a2e640e0b62a673ac2515bffb5
César O. Araújo <oaraujocesar@gmail.com>
2021-12-30 12:14:11 -0300

    feat: :sparkles: Create logo component

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Logo from './Logo'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  args: {}
+} as ComponentMeta<typeof Logo>
+
+export const Basic: ComponentStory<typeof Logo> = (args) => <Logo {...args} />
+
+Basic.args = {}
