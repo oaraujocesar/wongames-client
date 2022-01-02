8cffa8b812fef537c432d2b2bbf5e14b98a045fd
César O. Araújo <oaraujocesar@gmail.com>
2022-01-01 21:09:14 -0300

    feat: :sparkles: Create the open/close menu logic

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Menu from './Menu'
 
@@ -12,5 +12,24 @@ describe('<Menu />', () => {
     expect(
       screen.getByLabelText(/ir para o carrinho de compras/i)
     ).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    fireEvent.click(screen.getByLabelText(/abrir menu/i))
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    fireEvent.click(screen.getByLabelText(/fechar menu/i))
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,29 +1,46 @@
+import { useState } from 'react'
+
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { Close as CloseIcon } from '@styled-icons/material-outlined'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 
-import { Wrapper, IconWrapper, LogoWrapper, MenuGroup } from './styles'
+import {
+  Wrapper,
+  IconWrapper,
+  LogoWrapper,
+  MenuGroup,
+  MenuFull
+} from './styles'
 import { Logo } from 'components/Logo/Logo'
 
-const Menu = () => (
-  <Wrapper>
-    <IconWrapper>
-      <MenuIcon aria-label="Abrir menu" />
-    </IconWrapper>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <LogoWrapper>
-      <Logo hideOnMobile />
-    </LogoWrapper>
+  return (
+    <Wrapper>
+      <IconWrapper>
+        <MenuIcon aria-label="Abrir menu" onClick={() => setIsOpen(true)} />
+      </IconWrapper>
 
-    <MenuGroup>
-      <IconWrapper>
-        <SearchIcon aria-label="Buscar" />
-      </IconWrapper>
-      <IconWrapper>
-        <ShoppingCartIcon aria-label="Ir para o Carrinho de Compras" />
-      </IconWrapper>
-    </MenuGroup>
-  </Wrapper>
-)
+      <LogoWrapper>
+        <Logo hideOnMobile />
+      </LogoWrapper>
+
+      <MenuGroup>
+        <IconWrapper>
+          <SearchIcon aria-label="Buscar" />
+        </IconWrapper>
+        <IconWrapper>
+          <ShoppingCartIcon aria-label="Ir para o Carrinho de Compras" />
+        </IconWrapper>
+      </MenuGroup>
+
+      <MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Fechar menu" onClick={() => setIsOpen(false)} />
+      </MenuFull>
+    </Wrapper>
+  )
+}
 
 export default Menu
