fda43d9e8b4599697dfbcd90bc8985a6c2ce5531
César O. Araújo <oaraujocesar@gmail.com>
2022-01-04 00:33:35 -0300

    fix: :bug: Disable eslint for jest-styled-component declaration file

--- src/types/definition/jest-styled-components.d.ts
+++ src/types/definition/jest-styled-components.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import { Plugin, NewPlugin } from 'pretty-format'
 import { css } from 'styled-components'
