e40e521666abad33c6cc1f3b7f52a2d9a5fa6b54
César O. Araújo <oaraujocesar@gmail.com>
2022-01-14 20:25:26 -0300

    feat: :sparkles: Create BannerSlider component

---
+++ src/components/BannerSlider/BannerSlider.stories.tsx
@@ -0,0 +1,45 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { BannerSlider } from './BannerSlider'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x581',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as ComponentMeta<typeof BannerSlider>
+
+export const Default: ComponentStory<typeof BannerSlider> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+)
---
+++ src/components/BannerSlider/BannerSlider.tsx
@@ -0,0 +1,36 @@
+import { Banner, BannerProps } from 'components/Banner/Banner'
+import { Slider, SliderSettings } from 'components/Slider/Slider'
+import { Wrapper } from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </Wrapper>
+)
+
+export { BannerSlider }
